ad6e5d23b91fb208d19239b03dafb4927705a59a
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-09 10:55:34 +0200

    Created conditions for inputs

--- scripts.js
+++ scripts.js
@@ -3,7 +3,20 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  if (!dividend || !divider) {
+    alert(
+      "Division not performed. Both values are required in inputs. Try again"
+    );
+  } else if (isNaN(parseFloat(dividend)) || isNaN(parseFloat(divider))) {
+    console.trace("Error: Entry is not a number");
+    document.body.innerHTML = `<h1 style="text-align: center;"> Something critical went wrong. Please reload the page! </h1>`;
+  } else if (divider == 0) {
+    console.trace("Error: Cant divide by zero");
+    alert("Division not performed. Invalid number provided. Try again");
+  } else {
+    result.innerText = Math.trunc(dividend / divider);
+  }
 });
