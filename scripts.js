b574cdbcaf870160239f6bece1d0181e81720a6f
Mr-Kasa <156632133+Mr-Kasa@users.noreply.github.com>
2024-05-09 11:52:10 +0200

    Fixed dispay errors

--- scripts.js
+++ scripts.js
@@ -7,16 +7,16 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   if (!dividend || !divider) {
-    alert(
-      "Division not performed. Both values are required in inputs. Try again"
-    );
+      result.innerText ="Division not performed. Both values are required in inputs. Try again"
+    ;
   } else if (isNaN(parseFloat(dividend)) || isNaN(parseFloat(divider))) {
     console.trace("Error: Entry is not a number");
     document.body.innerHTML = `<h1 style="text-align: center;"> Something critical went wrong. Please reload the page! </h1>`;
   } else if (divider == 0) {
     console.trace("Error: Cant divide by zero");
-    alert("Division not performed. Invalid number provided. Try again");
+    result.innerText ="Division not performed. Invalid number provided. Try again";
   } else {
     result.innerText = Math.trunc(dividend / divider);
   }
 });
+
